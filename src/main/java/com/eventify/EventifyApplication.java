4f486f6aadb478c7f10f60f0947a3e06fd8880d1
Vuk Trifunovic <trifunovicvuk03@gmail.com>
2025-08-26 14:00:00 +0200

    chore: Finalize project structure and add API test file

---
+++ src/main/java/com/eventify/EventifyApplication.java
@@ -0,0 +1,10 @@
+package com.eventify;
+
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventifyApplication.class, args);
+    }
+}
